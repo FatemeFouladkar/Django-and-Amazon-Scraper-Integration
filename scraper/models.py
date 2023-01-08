0bc8bd1669be15cb1ef34a36ed4e961535f34380
Fateme Fouladkar <fatemefuoladkar@gmail.com>
2023-01-08 09:57:59 +0330

    add products model

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class AmazonPhoneProducts(models.Model):
+    link = models.URLField(null=True, blank=True, verbose_name=_('link'))
+    title = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('title'))
+    price = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('price'))
+    rating = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('rating'))
+    brand = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('brand'))
+    model_name = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('model name'))
+    operating_system = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('operating system'))
+    cellular_technology = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('cellular technology'))
+    wireless_network_technology = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('wireless network technology'))
+    about = models.TextField(null=True, blank=True, verbose_name=_('about'))
