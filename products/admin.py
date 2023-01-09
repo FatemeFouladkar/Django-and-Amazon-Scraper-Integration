a532749dfbceb0db61e64acd8a23a9d830f58fbe
Fateme Fouladkar <fatemefuoladkar@gmail.com>
2023-01-09 14:45:29 +0330

    add start scraping button to the admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,8 @@
+import os
 from django.contrib import admin
+from django.urls import path
+from django.shortcuts import redirect
+from django.contrib import messages
 
 from .models import AmazonPhoneProducts, InputLinks
 
@@ -10,7 +14,21 @@ class AmazonPhoneProductsAdmin(admin.ModelAdmin):
 
     search_fields = ('ASIN',)
 
-
 @admin.register(InputLinks)
 class InputLinksAdmin(admin.ModelAdmin):
     list_display = ('title', 'scrape',)
+    
+    change_list_template = 'start_scraping.html'
+
+    def get_urls(self):
+        return [
+            path('start_scraping/', self.start_scraping)
+        ] + super().get_urls()
+
+    def start_scraping(self, request):
+        if request.method == 'GET':
+            os.system("python manage.py crawl")
+            self.message_user(self, request,
+                             'Crawler initialized successfully',
+                             level=messages.SUCCESS)
+        return redirect('../')
